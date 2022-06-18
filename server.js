5eba77de61d349a43ca0e001e509d4ae8a8bb58f
DanOsad <dan@osadtsuk.com>
2022-06-18 18:28:59 +0300

    complete rebuild, working node-fetch call

--- server.js
+++ server.js
@@ -1,67 +1,28 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args))
-require('dotenv').config()
-
 const app = express()
-const PORT = process.env.PORT
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args))
+const cors = require('cors')
+require('dotenv').config()
+const PORT = 8000
 
-app.use(express.static('public'))
-app.use(bodyParser.json()) 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(express.json())
 
-let addresses = []
-
-// if (!localStorage.getItem('Wallets')) {
-//     localStorage.setItem('Wallets', [])
-// }
-
-app.listen(PORT, console.log(`Listening on port ${PORT}`))
-
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html')
-})
-
-async function apiCall(addresses, key){
-    let address = ''
-    addresses.forEach(wallet=>address += `${wallet},`)
-    const url = `https://api.etherscan.io/api?module=account&action=balancemulti&address=${address}&tag=latest&apikey=${key},` 
-    try{
-        const response = await fetch(url)
-        const data = await response.json()
-        for (let i=0; i<addresses.length; i++) {
-            console.log(`Address: ${data.result[i]['account']}, Balance: ${data.result[i]['balance']}`)
+app.get('/api/:addresses', async (request, response) => {
+    const key = process.env.KEY
+    const address = request.params.addresses
+    async function apiCall(key, addresses) {
+        try{
+            const response = await fetch(`https://api.etherscan.io/api?module=account&action=balancemulti&address=${addresses}&tag=latest&apikey=${key}`)
+            const data = await response.json()
+            return data
+        }catch(error){
+            console.log(error)
         }
-    }catch(error){
-        console.log(error)
     }
-}
-
-async function getEthPrice(){
-    const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ethereum"
-    try{
-        const response = await fetch(url)
-        const data = await response.json()
-        console.log(data[0]['current_price'])
-    }catch(error){
-        console.log(error)
-    }
-}
-
-app.post('/api', (req,res) => {
-    const eth_key = process.env.KEY
-    const wallets = req.body.wallets
-    const ethPrice = getEthPrice()
-    // apiCall(wallets, eth_key)
-
+    response.json(await apiCall(key, address))
 })
 
-/* 
-
-1) front end makes '/api' get request
-2) store in wallets in localstorage
-3) make etherscan api call
-4) make coingecko api call
-5) display values in DOM
-
-*/
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Listening on port: ${PORT}...`)
+})
